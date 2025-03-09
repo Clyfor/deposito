cf1314df22a5fe9fef1f87d489c4dafabea483ce
Sergio <sergio_22-09-92@hotmail.com>
2025-03-09 15:19:50 +0100

    MASTER

---
+++ CCuenta.java
@@ -0,0 +1,133 @@
+/**
+ * Clase que representa una cuenta bancaria con operaciones de ingreso y retiro.
+ * Permite consultar el estado de la cuenta y modificar sus atributos.
+ * 
+ * @author Sergio Ramírez Pla
+ * @version 1.1
+ */
+public class CCuenta {
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    /**
+     * Obtiene el nombre del titular de la cuenta.
+     * @return Nombre del titular.
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Establece el nombre del titular de la cuenta.
+     * @param nombre Nombre del titular.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Obtiene el número de cuenta.
+     * @return Número de cuenta.
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * Establece el número de cuenta.
+     * @param cuenta Número de cuenta.
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * Obtiene el saldo actual de la cuenta.
+     * @return Saldo actual.
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Establece el saldo de la cuenta.
+     * @param saldo Nuevo saldo de la cuenta.
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * Obtiene el tipo de interés aplicado a la cuenta.
+     * @return Tipo de interés.
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * Establece el tipo de interés de la cuenta.
+     * @param tipoInterés Nuevo tipo de interés.
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+    /**
+     * Constructor por defecto de la clase CCuenta.
+     */
+    public CCuenta() {
+    }
+
+    /**
+     * Constructor con parámetros para inicializar la cuenta.
+     * @param nom Nombre del titular.
+     * @param cue Número de cuenta.
+     * @param sal Saldo inicial.
+     * @param tipo Tipo de interés.
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        this.nombre = nom;
+        this.cuenta = cue;
+        this.saldo = sal;
+        this.tipoInterés = tipo;
+    }
+
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     * @return Saldo actual de la cuenta.
+     */
+    public double estado() {
+        return saldo;
+    }
+
+    /**
+     * Método para ingresar dinero en la cuenta.
+     * @param cantidad Cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        saldo += cantidad;
+    }
+
+    /**
+     * Método para retirar dinero de la cuenta.
+     * @param cantidad Cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o supera el saldo disponible.
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No hay suficiente saldo");
+        }
+        saldo -= cantidad;
+    }
+}
